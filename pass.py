202fa8c21ee558320286c1a8ef6b785123646218
tarkan <tarknsahin@gmail.com>
2022-03-22 12:54:49 +0100

    it is an assignment regardin password reminder

---
+++ pass.py
@@ -0,0 +1,7 @@
+pasword="w@12"
+name="tarkan"
+user=input("please enter your name")
+if user.lower()=="tarkan":
+  print("here is the password:" + pasword)
+else:
+ print( "Hello "+ user + " see you later.")
